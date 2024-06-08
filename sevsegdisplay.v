491e9d4acf45ce39a4ba96a84703e24b48f5c635
amkrishnan28 <99427361+amkrishnan28@users.noreply.github.com>
2024-06-07 20:51:51 -0700

    Update sevsegdisplay.v

--- sevsegdisplay.v
+++ sevsegdisplay.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/20/2024 03:42:07 PM
-// Design Name: 
-// Module Name: sevsegdisplay
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module sevsegdisplay(
