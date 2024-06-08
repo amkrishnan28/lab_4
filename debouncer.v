e04c6be8a296d2e8788b47062d11fecf513e2294
amkrishnan28 <99427361+amkrishnan28@users.noreply.github.com>
2024-06-07 20:51:03 -0700

    Update debouncer.v

--- debouncer.v
+++ debouncer.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/23/2024 11:37:18 PM
-// Design Name: 
-// Module Name: debouncer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module debouncer(input button, input clk, output btn_state);
 reg tempstate = 0;
 reg [15:0] counter;
